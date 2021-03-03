4e53e29d9ee65b9b11fe10862794f200905b7afa
waidelotte <waidelotte.advitam@gmail.com>
2021-03-03 15:07:12 +0200

    Add project files.

---
+++ WDLT.Clients.Youtube/Models/YoutubeSW.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace WDLT.Clients.Youtube.Models
+{
+    public class YoutubeSW
+    {
+        [JsonProperty("INNERTUBE_API_KEY")]
+        public string InnertubeApiKey { get; set; }
+
+        [JsonProperty("INNERTUBE_API_VERSION")]
+        public string InnertubeApiVersion { get; set; }
+
+        [JsonProperty("INNERTUBE_CLIENT_NAME")]
+        public string InnertubeClientName { get; set; }
+
+        [JsonProperty("INNERTUBE_CLIENT_VERSION")]
+        public string InnertubeClientVersion { get; set; }
+
+        [JsonProperty("INNERTUBE_CONTEXT")]
+        public YoutubeSWContext InnertubeContext { get; set; }
+
+        [JsonProperty("INNERTUBE_CONTEXT_CLIENT_NAME")]
+        public long InnertubeContextClientName { get; set; }
+
+        [JsonProperty("INNERTUBE_CONTEXT_CLIENT_VERSION")]
+        public string InnertubeContextClientVersion { get; set; }
+    }
+
+    public class YoutubeSWClient
+    {
+        [JsonProperty("clientName")]
+        public string ClientName { get; set; }
+
+        [JsonProperty("clientVersion")]
+        public string ClientVersion { get; set; }
+    }
+
+    public class YoutubeSWContext
+    {
+        [JsonProperty("client")]
+        public YoutubeSWClient Client { get; set; }
+    }
+}
---
+++ WDLT.Clients.Youtube/YoutubeClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+using WDLT.Clients.Base;
+using WDLT.Clients.Youtube.Models;
+
+namespace WDLT.Clients.Youtube
+{
+    public class YoutubeClient : BaseClient
+    {
+        public YoutubeClient(string userAgent) : base("https://www.youtube.com", userAgent)
+        {
+
+        }
+
+        public async Task<YoutubeSW> SW()
+        {
+            var raw = await RequestRawAsync(new RestRequest("/sw.js"));
+            var json = Regex.Match(raw.Content, @"(?<=ytcfg\.set\()(.*)(?=\);importScripts)").Value;
+            return JsonConvert.DeserializeObject<YoutubeSW>(json);
+        }
+
+        public async Task<string> ChannelId(string url)
+        {
+            var response = await RequestRawAsync(new RestRequest(url));
+
+            return Regex.Match(response.Content, "(?<=browseId\":\")([^\".]*)(?=\")").Value;
+        }
+    }
+}
